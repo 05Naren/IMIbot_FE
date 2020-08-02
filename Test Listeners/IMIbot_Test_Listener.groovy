8dcbcaed18f772fb74eae2833d935059066b6697
Narendra Nag <narendra.n@imimobile.com>
2020-08-02 20:20:48 +0530

    initial commit

--- Test Listeners/IMIbot_Test_Listener.groovy
+++ Test Listeners/IMIbot_Test_Listener.groovy
@@ -1,26 +1,14 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import org.openqa.selenium.Capabilities
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.remote.RemoteWebDriver
+import com.kms.katalon.core.annotation.AfterTestSuite
+import com.kms.katalon.core.annotation.BeforeTestCase
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 
 import internal.GlobalVariable as GlobalVariable
-
-import com.kms.katalon.core.annotation.BeforeTestCase
-import com.kms.katalon.core.annotation.BeforeTestSuite
-import com.kms.katalon.core.annotation.AfterTestCase
-import com.kms.katalon.core.annotation.AfterTestSuite
-import com.kms.katalon.core.context.TestCaseContext
-import com.kms.katalon.core.context.TestSuiteContext
 
 class IMIbot_Test_Listener {
 
@@ -34,20 +22,15 @@ class IMIbot_Test_Listener {
 		WebUI.maximizeWindow()
 	}
 
-	/*@BeforeTestSuite
-	def testSetUp(){
-		WebUI.openBrowser(GlobalVariable.URL)
-
-		WebUI.maximizeWindow()
-
-		WebUI.sendKeys(findTestObject('Input/username'), GlobalVariable.USERNAME)
-
-		WebUI.sendKeys(findTestObject('Input/password'), GlobalVariable.PASSWORD)
-
-		WebUI.click(findTestObject('Button/signIn'))
-
-		if (!(WebUI.waitForElementPresent(findTestObject('Button/createBot'), 30, FailureHandling.OPTIONAL))) {
-			WebUI.waitForPageLoad(10)
-		}
-	}*/
+	@AfterTestSuite
+	def displayExecutionInfo(){
+		WebDriver driver = DriverFactory.getWebDriver()
+		KeywordUtil.logInfo('BROWSER NAME:' + DriverFactory.getExecutedBrowser().getName())
+		KeywordUtil.logInfo('BROWSER VERSION:' + DriverFactory.getBrowserVersion(driver))
+		KeywordUtil.logInfo('ENVIRONMENT:' + RunConfiguration.getExecutionProfile())
+		KeywordUtil.logInfo('0S:' + RunConfiguration.getOS())
+		KeywordUtil.logInfo('ENVIRONMENT:' + RunConfiguration.getHostName())
+		Capabilities cap = ((RemoteWebDriver)driver).getCapabilities()
+		KeywordUtil.logInfo(cap.getBrowserName().toString()+' :'+ cap.getVersion().toString())
+	}
 }
